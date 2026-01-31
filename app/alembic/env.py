1766a69ce7870ac23e30c4f0f361bc9d8536a7be
srt-2000 <srt2000888@gmail.ru>
2026-01-31 15:23:04 +0300

    added cascade deleting for models relationships one-to-many, corrected alembic/env.py(added models import, in base migration version made changes and made downgrade/upgrade migrations

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -19,9 +19,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-sys.path.insert(0, dirname(dirname(abspath(__file__))))
-
 from app.database import DATABASE_URL, BaseSQLModel
+import app.models.models  # noqa: F401
 
 # This is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
